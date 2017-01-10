b43e69aeaf77115dd44b6ecd3b628f59b3dd4395
Joe Gigs <jgiglio86@gmail.com>
2017-01-09 19:42:15 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+ $(function(){
+        
+        $("#image").fadeOut(0)
+          .fadeIn();
+        
+        $("#soundcloud").fadeOut(0)
+          .delay(500)
+          .fadeIn();
+        
+        $("#news").slideUp(0)
+          .delay(1000)
+          .slideDown();
+          
+      });
